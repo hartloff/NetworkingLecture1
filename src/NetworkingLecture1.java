937cf427ca72b1b1e330645b8cb310b7d6bdb227
jessehartloff <hartloff@buffalo.edu>
2017-04-02 10:29:52 -0400

    initial commit

---
+++ src/NetworkingLecture1.java
@@ -0,0 +1,17 @@
+
+public class NetworkingLecture1 {
+
+
+    // 5 points
+    static String Q1() {
+        // Send an HTTP GET request to http://fury.cse.buffalo.edu/api/lecture1 and return the server's response
+
+        return "";
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
